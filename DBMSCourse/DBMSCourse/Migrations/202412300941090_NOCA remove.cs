d06ec92c49c9c634ae603ced61ab238940191660
Harun Yahya Ünal <yahyaunal89@gmail.com>
2024-12-30 12:42:30 +0300

    NOCA remove migration

---
+++ DBMSCourse/DBMSCourse/Migrations/202412300941090_NOCA remove.cs
@@ -0,0 +1,18 @@
+﻿namespace DBMSCourse.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NOCAremove : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Sections", "NumberOfCorrectAnswers");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Sections", "NumberOfCorrectAnswers", c => c.Int(nullable: false));
+        }
+    }
+}
--- DBMSCourse/DBMSCourse/Models/Section.cs
+++ DBMSCourse/DBMSCourse/Models/Section.cs
@@ -12,7 +12,6 @@ namespace DBMSCourse.Models
         public int SectionId { get; set; }
         public string SectionName { get; set; }
         public string DetailedInfo { get; set; }
-        public int NumberOfCorrectAnswers { get; set; }
     }
 
 }
